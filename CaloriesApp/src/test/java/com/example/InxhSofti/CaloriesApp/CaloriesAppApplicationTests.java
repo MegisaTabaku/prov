bf63dbdee1e45f8312cbdd98adb13ae56be9e65a
MegisaTabaku <tabakumegisa@gmail.com>
2025-01-15 22:55:20 +0100

    Add files via upload

---
+++ CaloriesApp/src/test/java/com/example/InxhSofti/CaloriesApp/CaloriesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InxhSofti.CaloriesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
