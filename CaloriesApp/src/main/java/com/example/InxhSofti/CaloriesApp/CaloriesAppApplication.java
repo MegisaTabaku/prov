e92caf944f9bd4b7b482e3733768ad61ef2606fc
Megisa Tabaku <tabakumegisa@gmail.com>
2025-01-16 11:55:53 +0100

    Hi

--- CaloriesApp/src/main/java/com/example/InxhSofti/CaloriesApp/CaloriesAppApplication.java
+++ CaloriesApp/src/main/java/com/example/InxhSofti/CaloriesApp/CaloriesAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaloriesAppApplication {
-	int kalori = 0;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CaloriesAppApplication.class, args);
 	}
